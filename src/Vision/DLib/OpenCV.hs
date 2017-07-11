b7b4118087ea2ce9811cc411b2721c3e8a32e37e
Jordan Medlock <jordanemedlock@gmail.com>
2017-07-11 01:06:11 +0000

    OpenCV support

---
+++ src/Vision/DLib/OpenCV.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE CPP #-}
+
+
+{-|
+Module      : Vision.DLib.OpenCV
+Description : OpenCV Iterfacing functions
+Copyright   : (c) Jordan Medlock, 2017
+Maintainer  : jordanemedlock@gmail.com
+Portability : POSIX
+
+-}
+module Vision.DLib.OpenCV where
+
+
+import qualified Language.C.Inline as C
+import qualified Data.ByteString.Char8 as BS
+
+import           Vision.DLib.Types.Array2D
+import           Vision.DLib.Types.InlineC
+import           Control.Processor
+import           System.FilePath.Posix ( takeExtension )
+import           Data.Char
+import           Foreign.Ptr
+
+C.context dlibCtx
+
+
+C.include "<string>"
+
+C.include "<dlib/image_processing.h>"
+C.include "<dlib/opencv.h>"
+C.include "<iostream>"
+C.include "typedefs.h"
+
+fromIplImage :: Ptr () -> IO Image
+fromIplImage cvImage = Image <$> [C.block| image * {
+  image * img = new dlib::array2d<dlib::rgb_pixel>();
+  dlib::cv_image<dlib::bgr_pixel> cvImage((IplImage *)$(void * cvImage));
+  dlib::assign_image(*img, cvImage);
+  return img;
+}|]
+
